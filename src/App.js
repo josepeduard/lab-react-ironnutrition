1255fe50267609698bc1760401a4991df9744ffe
josepeduard <j.e.garriga.obach@gmail.com>
2019-03-20 15:29:10 +0100

    Iteration 2 | Display food

--- src/App.js
+++ src/App.js
@@ -2,45 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 import foods from './data/foods.json';
+import FoodBox from './components/FoodBox';
 
 class App extends Component {
+  state = {
+    foodList: foods
+  }
+  
   render() {
     return (
-      <div className="App">
-       <div className="box">
-          <article className="media">
-            <div className="media-left">
-              <figure className="image is-64x64">
-                <img src="https://i.imgur.com/eTmWoAN.png" />
-              </figure>
-            </div>
-        <div className="media-content">
-          <div className="content">
-            <p>
-              <strong>Pizza</strong> <br />
-              <small>400 cal</small>
-            </p>
-          </div>
-        </div>
-            <div className="media-right">
-              <div className="field has-addons">
-                <div className="control">
-                  <input
-                    className="input"
-                    type="number" 
-                    value="1"
-                  />
-                </div>
-                <div className="control">
-                  <button className="button is-info">
-                    +
-                  </button>
-                </div>
-              </div>
-            </div>
-          </article>
-        </div>
-      </div>
+      <div className = "box">
+        {this.state.foodList.map((item) => {
+          return <FoodBox 
+            name={item.name}
+            image={item.image}
+            calories={item.calories}
+            />})}
+      </div>      
     );
   }
 }
